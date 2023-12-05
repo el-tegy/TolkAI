5e46702558ff148ce636eff6a42b02a3a8768ee8
Elisee TEGUE YOMBI <elisee.tegue@groupe-esigelec.org>
2023-12-05 15:21:59 +0100

    Refactor code to improve performance and readability

--- image_search.py
+++ image_search.py
@@ -2,9 +2,15 @@ import requests
 
 url = "https://www.googleapis.com/customsearch/v1"
 params = {
+<<<<<<< HEAD
     "q": "An image showing how to get data with power bi desktop",
     "cx": "d5a58fd2521514055",
     "key": "AIzaSyANitOObhh9yTC7Sd6GdiLQGcLJgI1Tz7E",
+=======
+    "q": "Image of the Power BI Desktop application with the 'Get Data' button highlighted",
+    "cx": <>,
+    "key": <>,
+>>>>>>> bcec05904a14f58989b4e4ef9a3fcdd29edc9ace
     "searchType": "image"
 }
 response = requests.get(url, params=params)

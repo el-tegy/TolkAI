462adf9e807d065b700e39265e680d56f23d209e
Elisee TEGUE YOMBI <elisee.tegue@groupe-esigelec.org>
2023-12-05 11:10:11 +0100

    fix params

--- image_search.py
+++ image_search.py
@@ -3,8 +3,8 @@ import requests
 url = "https://www.googleapis.com/customsearch/v1"
 params = {
     "q": "Image of the Power BI Desktop application with the 'Get Data' button highlighted",
-    "cx": "a46073abb1acd4346",
-    "key": "AIzaSyANitOObhh9yTC7Sd6GdiLQGcLJgI1Tz7E",
+    "cx": <>,
+    "key": <>,
     "searchType": "image"
 }
 response = requests.get(url, params=params)

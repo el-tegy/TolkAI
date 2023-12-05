a4a200c6a82f0ad04f940e600815f18383a47129
Elisee TEGUE YOMBI <elisee.tegue@groupe-esigelec.org>
2023-12-05 15:20:33 +0100

    Update image search.py and add image search.ipynb

--- image_search.py
+++ image_search.py
@@ -2,13 +2,19 @@ import requests
 
 url = "https://www.googleapis.com/customsearch/v1"
 params = {
-    "q": "Image of the Power BI Desktop application with the 'Get Data' button highlighted",
-    "cx": "a46073abb1acd4346",
+    "q": "An image showing how to get data with power bi desktop",
+    "cx": "d5a58fd2521514055",
     "key": "AIzaSyANitOObhh9yTC7Sd6GdiLQGcLJgI1Tz7E",
     "searchType": "image"
 }
 response = requests.get(url, params=params)
 data = response.json()
 
-# Process and use the data as needed
-print(data['items'])
+items = data['items']
+items_links=[item['link'] for item in items]
+print(items_links)
+for link in items_links:
+    items_and_links={}
+    items_and_links[link]=link.split('https://learn.microsoft.com/en-us/power-bi/')[1].split('/')
+    print(items_and_links)
+    

4f0c5b43a92363e0895b373e05c4b6b9db36265a
jeffersonAkp <jeffersonjsnakpovo@gmail.com>
2023-12-22 15:12:17 +0100

    update of gemini pro model instantiation

--- chat_gemini.py
+++ chat_gemini.py
@@ -1,38 +1,28 @@
 import vertexai
 from vertexai.preview.generative_models import GenerativeModel
 
-
-
-#This function is responsible for generating the response
-def gemini_pro_call(prompt: str, **kwargs) -> str:
+def chat_gemini(query: str) -> str:
     """
     Generate a response based on the prompt using the Gemini Pro model.
 
     Args:
-        prompt (str): The input prompt for the language model.
-        **kwargs (Any): Additional keyword arguments.
-
+        query (str): The user's input/query.
+        
     Returns:
         str: The generated response by the language model.
+             (The model's response to the user's input.)
     """
+    # Initialize the Vertex AI SDK
+    vertexai.init(project="ping38", location="europe-west9")
+
+    # Create an instance of the GenerativeModel with the Gemini Pro model
     gemini_pro_model = GenerativeModel("gemini-pro")
+
+    # Generate content using the Gemini Pro model with the specified configuration
     model_response = gemini_pro_model.generate_content(
-        prompt,
-        generation_config={"temperature": 0.1}
+        query,
+        generation_config={"temperature": 0.2}
     )
+
+    # Extract and return the text content from the generated response
     return model_response.candidates[0].content.parts[0].text
-
-
-#This funtion is responsible for calling the generation function and retrieving the response.
-def chat_gemini(query: str) -> str:
-    """
-    A function to interact with the Gemini Pro language model.
-
-    Args:
-        query (str): The user's input/query.
-
-    Returns:
-        str: The model's response to the user's input.
-    """
-    response = gemini_pro_call(query)
-    return response

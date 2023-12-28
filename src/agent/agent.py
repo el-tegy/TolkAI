62b1141d3a207ecc60fb85cdbf5f29343d908799
Elisee TEGUE YOMBI <elisee.tegue@groupe-esigelec.org>
2023-12-28 01:39:07 +0100

    add an agent with google search functionality and image retrieval pipeline

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -1,26 +1,43 @@
 # Import necessary modules and classes
 from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent
-from langchain import SerpAPIWrapper, LLMChain
+from langchain.utilities import SerpAPIWrapper
+from langchain_community.utilities.google_search import GoogleSearchAPIWrapper
+from langchain.chains import LLMChain
 from template.template import CustomPromptTemplate, read_template
-from langchain.chat_models import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from parser.parser import CustomOutputParser
 from dotenv import load_dotenv
 from datetime import datetime
 from pathlib import Path
 from utils.config import load_config
+from image_retrieval import image_retrieval_pipeline
 
 # Load configuration from config.yml
 config = load_config()
 
 def setup_agent(chatbot_name):
     # Instantiate a SerpAPIWrapper object for search functionality
-    search = SerpAPIWrapper()
+    #search = SerpAPIWrapper(serpapi_api_key="65f72e3a8a483e9384510c99144a93f4f5e98d39da2775d519c7ff09101da23d")
+    search = GoogleSearchAPIWrapper(
+        google_api_key = "AIzaSyANitOObhh9yTC7Sd6GdiLQGcLJgI1Tz7E",
+        google_cse_id = "b3cc7e87732c140e9",
+        k=10
+    )
     # Instantiate a datetime object for datetime functionality
     tools = [
         Tool(
             name="Search",
             func=search.run,
-            description="The Search tool uses SerpAPI to conduct Google searches. It retrieves raw search results without any inherent interpretation. Utilize this tool to fetch real-time information, but always analyze and deduce relevant details from the results, avoiding the use of generic placeholders."
+            description="The Search tool uses Google Search API to conduct Google searches. It retrieves raw search results without any inherent interpretation. \
+            Utilize this tool to fetch links of images you need to enhance your answer, but always analyze and deduce relevant images from the results \
+            to avoid the use of generic placeholders such as '[Image of the 'get data' button in Power BI]'."
+        ),
+        Tool(
+            name="Image link from image label",
+            func=image_retrieval_pipeline,
+            description="This tool returns a image link given an image label passed as a parameter. \
+            Utilize this tool to fetch links of images you need to enhance your answer, by passing it images labels \
+            such as 'Image of the 'get data' button in Power BI'."
         ),
     ]
 
@@ -35,7 +52,7 @@ def setup_agent(chatbot_name):
     output_parser = CustomOutputParser()
 
     # Instantiate a ChatOpenAI object for language model interaction
-    llm = ChatOpenAI(temperature=config["OpenAI"]["llm_temp"])
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key = "AIzaSyANitOObhh9yTC7Sd6GdiLQGcLJgI1Tz7E")
 
     # Set up the LLMChain using the ChatOpenAI object and prompt template
     llm_chain = LLMChain(llm=llm, prompt=prompt)
@@ -64,3 +81,20 @@ def chat_with_agent(user_input: str, chatbot_name: str):
         return response.get("output")
     else:
         return response
+
+if __name__: 
+    # Load environment variables from .env file
+    load_dotenv(config["Key_File"])
+    # Get the chatbot name from the config.yml file
+    chatbot_name = "TolkAI"
+    # Get the user input from the user
+    user_input = "provide me with a step by step guide on how to create a time series in Tableau desktop. In your answer, \
+    include relevant images showing me where to click in Power BI so that I can easily follow up"
+    #user_input = "Who are you?"
+    #user_input = "Give me the link of an image of the Eiffel Tower. The link, not the image itself."
+    #user_input = "why aren't you able to perform Google searches and retrieve information from external websites?"
+    #user_input = "Where and when was the COP28 held?"
+    # Get the response from the agent
+    response = chat_with_agent(user_input, chatbot_name)
+    # Print the response
+    print(response)

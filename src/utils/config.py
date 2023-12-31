c3ddc6c126581de653b1c8ec1e0571606df41f10
Elisee TEGUE YOMBI <elisee.tegue@groupe-esigelec.org>
2023-12-31 15:21:22 +0100

    Remove serpapi_api_key from config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -18,7 +18,7 @@ def setup_environment_variables(config):
     load_dotenv(config["Key_File"])
     #openai_api_key = os.getenv('OPENAI_API_KEY')
     #serpapi_api_key = os.getenv('SERPAPI_API_KEY')
-    serpapi_api_key = "65f72e3a8a483e9384510c99144a93f4f5e98d39da2775d519c7ff09101da23d"
+    #serpapi_api_key = <>
 
     #if openai_api_key is not None:
     #    os.environ['OPENAI_API_KEY'] = openai_api_key

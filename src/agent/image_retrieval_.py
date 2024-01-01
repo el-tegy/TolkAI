56b08420a955f3029e617b6c7f20b78b8904e591
ongolo <christopherongolo@yahoo.com>
2024-01-01 19:03:37 +0100

    fix image retrieval

--- src/agent/image_retrieval_.py
+++ src/agent/image_retrieval_.py
@@ -11,22 +11,16 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 from vertexai.preview.generative_models import GenerativeModel
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed , wait
 from embedding_prediction_client import EmbeddingPredictionClient
 
 load_dotenv()
 #from IPython.display import Image, display
 PROJECT_ID = 'ping38'
 url = "https://www.googleapis.com/customsearch/v1"
-total_images = 20 # Total number of images to download
+total_images = 15 # Total number of images to download
 images_per_request = 5  # Maximum number of images per request
-params = {
-    "cx": os.getenv("Google_CSE_ID"),
-    "key": os.getenv("Google_API_Key"),
-    "searchType": "image",
-    "num": images_per_request,
-    "fileType": "BMP, GIF, JPEG, PNG"
-}
+
 client = EmbeddingPredictionClient(project=PROJECT_ID)
 
 
@@ -34,14 +28,22 @@ def image_search(query):
     all_images = []  # List to hold all the images
 
     for start_index in range(1, total_images + 1, images_per_request):
+        params = {
+            "q": query,
+            "cx": os.getenv("Google_CSE_ID"),
+            "key": os.getenv("Google_API_Key"),
+            "searchType": "image",
+            "num": images_per_request,
+            "start": start_index,
+            "fileType": "BMP, GIF, JPEG, PNG"
+        }
         response = requests.get(url, params=params)
         data = response.json()
-        params["q"] = query
-        params["start"] = start_index
+        
         if 'items' in data:
-            all_images.extend(item['link'] for item in data['items'])
+              all_images.extend(item['link'] for item in data['items'])
         else:
-            pass
+            print('an error occured while searching')
     return all_images
 
 def generate(image_link):
@@ -56,7 +58,7 @@ def generate(image_link):
     """
     model = GenerativeModel("gemini-pro-vision")
     responses = model.generate_content(
-        [image_link, """Give me a very brief description of this image"""],
+        [image_link, """Give me a very brief description of the purpose of this image"""],
         generation_config={
             "max_output_tokens": 1024,
             "temperature": 0.4,
@@ -71,15 +73,22 @@ def generate(image_link):
 def image_captioning_parallel(items_links):
     captions = {}
     with ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_url = {executor.submit(generate, url): url for url in items_links}
-        for future in as_completed(future_to_url):
-            url = future_to_url[future]
+      # Soumettre toutes les tâches en une seule étape
+
+        futures = {executor.submit(generate, url): url for url in items_links}
+        #print(futures)
+        
+        # Utilisation de as_completed pour itérer sur les futures au fur et à mesure qu'ils sont terminés
+        for future in as_completed(futures):
+            url = futures[future]
             try:
                 caption = future.result()
-            except:
+            except Exception as e:
                 pass
+                # Si vous voulez voir la trace complète de l'exception, vous pouvez utiliser traceback
             else:
                 captions[url] = caption
+    # À ce stade, toutes les tâches sont terminées
     return captions
 
 def getTextEmbedding(client, text):
@@ -101,11 +110,11 @@ def image_selection(query, client, data):
     query_vect = getTextEmbedding(client, query)
     query_vect = np.array(query_vect).reshape(1, -1)  # Reshape to 2D array
 
-    # Get embeddings for each caption and compute cosine similarity
+        # Get embeddings for each caption and compute cosine similarity
     data['embedding'] = data['Caption'].apply(lambda x: getTextEmbedding(client, x))
     data['cos_sim'] = data['embedding'].apply(lambda x: cosine_similarity(query_vect, np.array(x).reshape(1, -1))[0][0])
 
-    # Sort by cosine similarity in descending order and get the most relevant item
+        # Sort by cosine similarity in descending order and get the most relevant item
     most_relevant_item = data.sort_values(by='cos_sim', ascending=False).iloc[0]
 
     return most_relevant_item['ImageURL']
@@ -114,10 +123,11 @@ def image_retrieval_pipeline(query):
     images = image_search(query)
     captions = image_captioning_parallel(images)
     df_items = pd.DataFrame(list(captions.items()), columns=['ImageURL', 'Caption'])
-    try: 
+    if not df_items.empty and len(df_items) > 0:
         return image_selection(query, client, df_items)
-    except:
-        return df_items[0]['ImageURL']
-    
-query = "'get data' in Power BI"
-print(image_retrieval_pipeline(query))
+    elif df_items.empty:
+        print("No items in the DataFrame.")
+        return None  # ou une valeur par défaut
+    elif len(df_items) == 0:
+        print("DataFrame has zero rows.")
+        return None  # ou une valeur par défaut

7d8d6e6af967066caebf1ca0cc1732aadbcc3c85
Elisee TEGUE YOMBI <elisee.tegue@groupe-esigelec.org>
2024-01-26 01:15:34 +0100

    Add OpenAI integration and callbacks to agent setup and chat functions

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -20,20 +20,29 @@ from pathlib import Path
 from utils.config import load_config
 from agent.image_retrieval import image_retrieval_pipeline
 from agent.codey import code_generation
+from langchain_openai import ChatOpenAI
 import google.generativeai as genai
 import streamlit as st
+from langchain.callbacks.manager import (
+    AsyncCallbackManager,
+    AsyncCallbackManagerForChainRun,
+    CallbackManager,
+    CallbackManagerForChainRun,
+    Callbacks,
+)
 
 # Access API key stored in Streamlit's secrets
 google_api_key = st.secrets["api_keys"]["GOOGLE_API_KEY"]
 google_cse_id = st.secrets["api_keys"]["GOOGLE_CSE_ID"]
 google_genai_api_key = st.secrets["api_keys"]["GOOGLE_GENAI_API_KEY"]
+openai_api_key = st.secrets["api_keys"]["OPENAI_API_KEY"]
 
 load_dotenv()
 # Load configuration from config.yml
 config = load_config()
 genai.configure(api_key=google_genai_api_key)
 
-def setup_agent(chatbot_name, memory):
+def setup_agent(chatbot_name, memory, callbacks):
     # Instantiate a SerpAPIWrapper object for search functionality
     search = GoogleSearchAPIWrapper(
         google_api_key = google_api_key,
@@ -79,8 +88,12 @@ def setup_agent(chatbot_name, memory):
                                 google_api_key=google_genai_api_key,
                                 temperature=0.1)
 
+    gpt3 = ChatOpenAI(model="gpt-3.5-turbo-16k-0613", 
+                    openai_api_key = openai_api_key  
+                    )
+
     # Set up the LLMChain using the ChatOpenAI object and prompt template
-    llm_chain = LLMChain(llm=llm, prompt=prompt)
+    llm_chain = LLMChain(llm=gpt3, prompt=prompt)
 
     # Extract tool names from the tools list
     tool_names = [tool.name for tool in tools]
@@ -93,34 +106,34 @@ def setup_agent(chatbot_name, memory):
     )
 
     # Create an AgentExecutor from the agent and tools with verbose output
-    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory)
+    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory, callbacks=callbacks)
     return agent_executor
 
 
-def chat_with_agent(user_input: str, chatbot_name: str, memory):
+def chat_with_agent(user_input: str, chatbot_name: str, memory, callbacks):
     # Set up the agent using the setup_agent() function
-    agent_executor = setup_agent(chatbot_name, memory)
-    # Execute the agent with the given user_input and get the response
-    response = agent_executor.run(user_input)
+    agent_executor = setup_agent(chatbot_name, memory, callbacks=callbacks)
+    # Execute the agent with the given user_input and get the responses
+    response = agent_executor.run(user_input, callbacks=callbacks)
     # If the response is a dictionary, return the 'output' value, otherwise, return the response itself
     if isinstance(response, dict):
         return response.get("output")
     else:
         return response
 
-
-"""if __name__:
+"""
+if __name__:
     # Load environment variables from .env file
     load_dotenv(config["Key_File"])
     # Get the chatbot name from the config.yml file
     chatbot_name = "TolkAI"
     # Get the user input from the user
-    #user_input = "Who are you?"
+    user_input = "Who are you?"
     #user_input = "provide me with a step by step guide on how to create a time series in Power BI. In your answer, \
     #include relevant images showing me where to click in Power BI so that I can easily follow up"
     #user_input = "What is the difference between a bar chart and a line chart?"
     #user_input = "Give me the python code to sum up all the elements in a list."
-    user_input = "Comment créer un diagramme en série temporelle dans Qlik Sense ?"
+    #user_input = "Comment créer un diagramme en série temporelle dans Qlik Sense ?"
     # Get the response from the agent
     response = chat_with_agent(user_input, chatbot_name)
     # Print the response

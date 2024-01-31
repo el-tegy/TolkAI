999afe1b86235454535d4a29a084dc8b669331ea
Elisee TEGUE YOMBI <elisee.tegue@groupe-esigelec.org>
2024-01-31 23:41:12 +0100

    add streaming and set temp to 0.1

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -91,7 +91,9 @@ def setup_agent(chatbot_name, memory, callbacks):
                                 temperature=0.1) 
 
     gpt4 = ChatOpenAI(model="gpt-4-0613", 
-                    openai_api_key = openai_api_key
+                    openai_api_key = openai_api_key,
+                    streaming = True,
+                    temperature=0.1
                     )
     
     gpt = ChatOpenAI(model="gpt-3.5-turbo-1106", 
